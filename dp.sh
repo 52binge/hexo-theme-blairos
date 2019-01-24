21fd701f7b16691efc30f8c448e68589579c7896
blair <blair.values@gmail.com>
2019-01-24 09:41:21 +0800

    add a article or modify resources

--- dp.sh
+++ dp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "start deploy my blog theme..."
+echo "start deploy my blog theme blairos..."
 git pull
 git status
 #git add *
 git status
 git commit -m "add a article or modify resources"
 git push origin master
-echo "successful commit to blog theme"
+echo "successful commit to blog theme blairos"
